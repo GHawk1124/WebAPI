df79be8563f42308e4398503e7af89d23a4710f3
GHawk <garrett1124@icloud.com>
2019-04-20 23:29:28 -0700

    Correcting bugs
    Added a verbose check, refresh button, data fetches work properly and the 'index' variable can be expanded upon to give even more information.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
-let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
+//let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
+let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=';
+let verboseContentUrl = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=';
 
 let userInput;
+let index = 0;
 
 function setup() {
   noCanvas();
@@ -9,13 +12,41 @@ function setup() {
   userInput.changed(goWiki);
 
   function goWiki() {
-      let term = userInput.value();
-      let url = searchUrl + term;
-      loadJSON(url, gotData, 'jsonp')
+    let term = userInput.value();
+    let url = searchUrl + term;
+    loadJSON(url, gotData, 'jsonp');
   }
 
   function gotData(data) {
     console.log(data);
-    console.log(data[1][0])
+    let htitle = data[1][index];
+    let title = data[1][index];
+    title = title.replace(/\s+/g, '%20');
+    htitle = htitle.replace('%20', ' ');
+    createP(htitle);
+    console.log(title);
+    let searchData = contentUrl + title
+    let verboseSearchData = verboseContentUrl + title;
+    if (document.getElementById('verbose').checked) {
+    	loadJSON(verboseSearchData, verboseResults, 'jsonp');
+    } else {
+    	loadJSON(searchData, results, 'jsonp');
+    }
+  }
+
+  function results(data) {
+  	let page = data.query.pages;
+  	let pageId = Object.keys(data.query.pages)[0];
+  	console.log(pageId);
+  	let content = page[pageId].extract/*revisions[0]['*']*/;
+  	console.log(content);
+  	createP(content);
+  	createP('<hr>');
+  }
+
+  function verboseResults(data) {
+  	let page = data.parse.text['*'];
+  	createP(page);
+  	createP('<hr>');
   }
 }
